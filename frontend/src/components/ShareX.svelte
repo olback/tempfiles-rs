b93574962b17037d7aef6771f72f87c808e5fbcf
Edwin Svensson <git@olback.net>
2020-04-30 19:46:39 +0200

    added short sharex description

--- frontend/src/components/ShareX.svelte
+++ frontend/src/components/ShareX.svelte
@@ -2,12 +2,22 @@
 
 <h1>ShareX</h1>
 
+<main>
+
+    ShareX custom uploader config: <a href="sharex.sxcu">{window.origin}/sharex.sxcu</a>
+
+</main>
+
 <style type="text/scss">
 
     @import 'src/scss/variables';
 
-    h1 {
+    h1, a {
         color: $accent;
+    }
+
+    main {
+        color: #fff;
     }
 
 </style>
