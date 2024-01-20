283f6fa7b028e3b9cb09e7df3fcd547a9bd0133c
Edwin Svensson <git@olback.net>
2024-01-20 02:25:23 +0100

    update build script

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -12,5 +12,10 @@ cd ..
 # Target x86-64-v3
 RUSTFLAGS="-Ctarget-cpu=x86-64-v3" cargo build --release --target x86_64-unknown-linux-gnu
 cp $TARGET_DIR/x86_64-unknown-linux-gnu/release/tempfiles-rs .
-docker build -t registry.olback.dev/olback/tempfiles-rs .
-docker push registry.olback.dev/olback/tempfiles-rs
+
+date=$(date +"%Y-%m-%d")
+
+docker build --pull --no-cache -t registry.olback.dev/olback/tempfiles-rs:$date .
+docker tag registry.olback.dev/olback/tempfiles-rs:$date registry.olback.dev/olback/tempfiles-rs:latest
+docker push registry.olback.dev/olback/tempfiles-rs:$date
+docker push registry.olback.dev/olback/tempfiles-rs:latest
